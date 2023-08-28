5a60424d4eca4403d0c3e096da525c1cadfc003f
Joshua Sigston <joshua.sigston@outlook.com>
2023-08-27 20:36:36 -0600

    fetch chngs

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { Link } from 'react-router-dom';
 
@@ -8,52 +8,59 @@ import '@splidejs/react-splide/css/sea-green';
 import '@splidejs/react-splide/css/core';
 
 function Popular() {
-    const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState([]);
+  console.log('popular');
+  useEffect(() => {
+    getRandomRecipes();
+  }, []);
 
-    useEffect(() => {
-        getRandomRecipes()
-    }, [])
+  const getRandomRecipes = async () => {
+    // const newRecipes = localStorage.getItem('recipes')
 
-    const getRandomRecipes = async () => {
-        // const newRecipes = localStorage.getItem('recipes')
+    // if (newRecipes) {
+    //     setRecipes(JSON.parse(newRecipes))
+    // } else {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${
+        import.meta.env.VITE_KEY
+      }&number=9`,
+    );
+    const data = await api.json();
+    console.log(data);
+    // localStorage.setItem('recipes', JSON.stringify(data.recipes));
 
-        // if (newRecipes) { 
-        //     setRecipes(JSON.parse(newRecipes))
-        // } else {
-            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_KEY}&number=9`)
-            const data = await api.json()
-
-            localStorage.setItem('recipes', JSON.stringify(data.recipes))
-
-            setRecipes(data.recipes)
-        // }
-    }
+    setRecipes(data.recipes);
+    // }
+  };
 
   return (
     <div>
-        <Splide className='splide' options={{
-            perPage: 4,
-            breakpoints: { 375: {perPage : 1,}, 900:{perPage: 2}},
-            pagination: false, 
-            drag: 'free', 
-            arrows: false, 
-            gap: '1em'
-        }}>
-            {recipes.map( (recipe) => {
-                return(
-                    <SplideSlide key={recipe.id}>
-                        <Link to={'/recipe/' + recipe.id}>
-                            <div className='card'>
-                                <img src={recipe.image} alt="" />
-                                <p>{recipe.title}</p>
-                            </div>
-                        </Link>
-                    </SplideSlide>
-                )
-            })}
-        </Splide>
+      <Splide
+        className="splide"
+        options={{
+          perPage: 4,
+          breakpoints: { 375: { perPage: 1 }, 900: { perPage: 2 } },
+          pagination: false,
+          drag: 'free',
+          arrows: false,
+          gap: '1em',
+        }}
+      >
+        {recipes.map((recipe) => {
+          return (
+            <SplideSlide key={recipe.id}>
+              <Link to={'/recipe/' + recipe.id}>
+                <div className="card">
+                  <img src={recipe.image} alt="" />
+                  <p>{recipe.title}</p>
+                </div>
+              </Link>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;
--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { Link } from 'react-router-dom';
 import '@splidejs/react-splide/css';
@@ -7,55 +7,61 @@ import '@splidejs/react-splide/css/sea-green';
 import '@splidejs/react-splide/css/core';
 
 function Veggie() {
-  const [vegan, setVegan] = useState([])
+  const [vegan, setVegan] = useState([]);
 
-    const getVegan = async () => {
-        // const newVeggies = localStorage.getItem('veggies');
-        // console.log(newVeggies)
+  const getVegan = async () => {
+    // const newVeggies = localStorage.getItem('veggies');
+    // console.log(newVeggies)
 
-        // if (newVeggies) { 
-        //     console.log(newVeggies)
-        //     setVegan(JSON.parse(newVeggies))
-        // } else {
-            const apiKey = import.meta.env.VITE_KEY;
-            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=9&tags=vegetarian`)
-            const data = await api.json()
+    // if (newVeggies) {
+    //     console.log(newVeggies)
+    //     setVegan(JSON.parse(newVeggies))
+    // } else {
+    const apiKey = import.meta.env.VITE_KEY;
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=9&tags=vegetarian`,
+    );
+    const data = await api.json();
 
-            localStorage.setItem('veggies', JSON.stringify(data.recipes))
+    // localStorage.setItem('veggies', JSON.stringify(data.recipes));
 
-            setVegan(data.recipes)
-        // }
-    }
+    setVegan(data.recipes);
+    // }
+  };
 
-    useEffect(() => {
-        getVegan()
-    }, [])
+  useEffect(() => {
+    getVegan();
+  }, []);
+
+  if (vegan.length <= 0) return <p>Loading...</p>;
 
   return (
     <div>
-        <Splide options={{
-            perPage: 4, 
-            breakpoints: { 375: {perPage : 1,}, 900:{perPage: 2}},
-            pagination: false, 
-            drag: 'free', 
-            arrows: false, 
-            gap: '1em'
-        }}>
-            {vegan.map( (recipe) => {
-                return(
-                    <SplideSlide key={recipe.id}>
-                        <Link to={'/recipe/' + recipe.id}>
-                            <div className='card'>
-                                <img src={recipe.image} alt="" />
-                                <p>{recipe.title}</p>
-                            </div>
-                        </Link>
-                    </SplideSlide>
-                )
-            })}
-        </Splide>
+      <Splide
+        options={{
+          perPage: 4,
+          breakpoints: { 375: { perPage: 1 }, 900: { perPage: 2 } },
+          pagination: false,
+          drag: 'free',
+          arrows: false,
+          gap: '1em',
+        }}
+      >
+        {vegan.map((recipe) => {
+          return (
+            <SplideSlide key={recipe.id}>
+              <Link to={'/recipe/' + recipe.id}>
+                <div className="card">
+                  <img src={recipe.image} alt="" />
+                  <p>{recipe.title}</p>
+                </div>
+              </Link>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
     </div>
-  )
+  );
 }
 
-export default Veggie
+export default Veggie;
